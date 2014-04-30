a4d960ce7e5c4acba2c1483dc3acdcc658f094c5
Adam Patterson <Adam Patterson>
2014-04-30 14:28:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FailForward::Application.config.secret_key_base = '754fa8d3683b4028e59ba1abd97181e768191bb27e7f659d04b0799d43518c17b4530471ded9412a10b0556e310f089b5f3e88c7fccbd9f680868ccc8113bef1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FailForward::Application.config.session_store :cookie_store, key: '_fail_forward_session'
