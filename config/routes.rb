e242ca66e54cc3c97d844a3b9f709fb5cad5eb41
Adam Patterson <Adam Patterson>
2014-05-05 13:27:23 -0400

    fixes errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ FailForward::Application.routes.draw do
 
   resources :posts, only:[:index]
 
+  match 'auth/:provider', as: 'signin', to: 'sessions#create', via: [:get, :post]
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
