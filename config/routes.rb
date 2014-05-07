b41b25773ad1267d6c9ceee1ed408a7ed4beb08e
Adam Patterson <Adam Patterson>
2014-05-06 20:45:33 -0400

    adds comments on posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ FailForward::Application.routes.draw do
 
   root 'homes#show'
 
-  resources :posts, only:[:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :posts, only:[:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only:[:new, :create, :show]
+  end
 
   match 'auth/:provider', as: 'signin', to: 'sessions#create', via: [:get, :post]
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
