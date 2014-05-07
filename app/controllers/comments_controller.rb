b8ee5c5349e62199a37cd32294f5df80f71cdc65
Adam Patterson <Adam Patterson>
2014-05-07 10:38:40 -0400

    fixes errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,10 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     post.comments.create(comment_params)
-    redirect_to post_path(post)
+    redirect_to post
   end
 
   def show
-
   end
 
   private
